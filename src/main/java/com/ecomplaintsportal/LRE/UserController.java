68ae93a211bb1fd6fce4877972a8df16c01274ea
Ayeshabegum <ayeshashaikh9161@gmail.com>
2026-02-27 12:47:52 +0530

    develop backend

---
+++ src/main/java/com/ecomplaintsportal/LRE/UserController.java
@@ -0,0 +1,29 @@
+package com.ecomplaintsportal.LRE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.ecomplaintsportal.LRE.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return userService.loginUser(user.getEmail(), user.getPassword());
+    }
+
+    @PutMapping("/forgot-password")
+    public String forgotPassword(@RequestBody User user) {
+        return userService.updatePassword(user.getEmail(), user.getPassword());
+    }
+}
